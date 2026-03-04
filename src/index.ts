ab6ff531eb6d4dd2d9279df1063bd9567a7514b3
silence_top <8461090+silence-top@user.noreply.gitee.com>
2026-03-04 15:24:30 +0800

    导出ScalebarType

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
-import { MedViewerEngine, type MedEngineOptions } from './core/Engine';
-import { type ScalebarOptions } from './core/Scalebar';
-import { AnnoAnnotator } from './core/AnnoAnnotator';
-import { MedToolbar, type ToolbarOptions } from './core/Toolbar';
-import { SelectionPlugin, type SelectionOptions } from './core/SelectionPlugin';
-import { ColorAdjustPlugin, type ColorAdjustOptions } from './core/ColorAdjustPlugin';
-import { MagnificationPlugin, type MagnificationOptions } from './core/Magnification';
+import { MedViewerEngine, type MedEngineOptions } from "./core/Engine";
+import {
+  type ScalebarOptions,
+  ScalebarType,
+  ScalebarLocation,
+} from "./core/Scalebar";
+import { AnnoAnnotator } from "./core/AnnoAnnotator";
+import { MedToolbar, type ToolbarOptions } from "./core/Toolbar";
+import { SelectionPlugin, type SelectionOptions } from "./core/SelectionPlugin";
+import {
+  ColorAdjustPlugin,
+  type ColorAdjustOptions,
+} from "./core/ColorAdjustPlugin";
+import {
+  MagnificationPlugin,
+  type MagnificationOptions,
+} from "./core/Magnification";
 
 // 1. 导出供模块化开发使用 (Vue/React/Vite)
-export { MedViewerEngine, type MedEngineOptions, AnnoAnnotator, MedToolbar, type ToolbarOptions, SelectionPlugin, type SelectionOptions, ColorAdjustPlugin, type ColorAdjustOptions, MagnificationPlugin, type MagnificationOptions, type ScalebarOptions };
+export {
+  MedViewerEngine,
+  type MedEngineOptions,
+  AnnoAnnotator,
+  MedToolbar,
+  type ToolbarOptions,
+  SelectionPlugin,
+  type SelectionOptions,
+  ColorAdjustPlugin,
+  type ColorAdjustOptions,
+  MagnificationPlugin,
+  type MagnificationOptions,
+  type ScalebarOptions,
+  ScalebarType,
+  ScalebarLocation,
+};
 
 // 2. 导出供原生 HTML 全局变量使用 (window.MedViewerSDK)
-if (typeof window !== 'undefined') {
+if (typeof window !== "undefined") {
   (window as any).MedViewerSDK = {
     MedViewerEngine,
     AnnoAnnotator,
     MedToolbar,
     SelectionPlugin,
     ColorAdjustPlugin,
-    MagnificationPlugin
+    MagnificationPlugin,
   };
 }
