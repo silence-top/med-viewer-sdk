ee109b66f2e3d36c1a03f057518734d7372f655c
silence_top <8461090+silence-top@user.noreply.gitee.com>
2026-03-04 14:21:56 +0800

    导出数据类型

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { MedViewerEngine } from './core/Engine';
+import { MedViewerEngine, type MedEngineOptions } from './core/Engine';
 // import { KonvaAnnotator } from './core/KonvaAnnotator';
 import { AnnoAnnotator } from './core/AnnoAnnotator';
-import { MedToolbar } from './core/Toolbar';
-import { SelectionPlugin } from './core/SelectionPlugin';
-import { ColorAdjustPlugin } from './core/ColorAdjustPlugin';
-import { MagnificationPlugin } from './core/Magnification';
+import { MedToolbar, type ToolbarOptions } from './core/Toolbar';
+import { SelectionPlugin, type SelectionOptions } from './core/SelectionPlugin';
+import { ColorAdjustPlugin, type ColorAdjustOptions } from './core/ColorAdjustPlugin';
+import { MagnificationPlugin, type MagnificationOptions } from './core/Magnification';
 
 // 1. 导出供模块化开发使用 (Vue/React/Vite)
-export { MedViewerEngine, AnnoAnnotator, MedToolbar, SelectionPlugin, ColorAdjustPlugin, MagnificationPlugin };
+export { MedViewerEngine, type MedEngineOptions, AnnoAnnotator, MedToolbar, type ToolbarOptions, SelectionPlugin, type SelectionOptions, ColorAdjustPlugin, type ColorAdjustOptions, MagnificationPlugin, type MagnificationOptions };
 
 // 2. 导出供原生 HTML 全局变量使用 (window.MedViewerSDK)
 if (typeof window !== 'undefined') {
