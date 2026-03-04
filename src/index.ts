559a02c76525fb29d2aa19ac96b73ac5ffb5e28a
silence_top <8461090+silence-top@user.noreply.gitee.com>
2026-03-04 14:33:40 +0800

    导出ScalebarOptions类型

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { MedViewerEngine, type MedEngineOptions } from './core/Engine';
-// import { KonvaAnnotator } from './core/KonvaAnnotator';
+import { type ScalebarOptions } from './core/Scalebar';
 import { AnnoAnnotator } from './core/AnnoAnnotator';
 import { MedToolbar, type ToolbarOptions } from './core/Toolbar';
 import { SelectionPlugin, type SelectionOptions } from './core/SelectionPlugin';
@@ -7,7 +7,7 @@ import { ColorAdjustPlugin, type ColorAdjustOptions } from './core/ColorAdjustPl
 import { MagnificationPlugin, type MagnificationOptions } from './core/Magnification';
 
 // 1. 导出供模块化开发使用 (Vue/React/Vite)
-export { MedViewerEngine, type MedEngineOptions, AnnoAnnotator, MedToolbar, type ToolbarOptions, SelectionPlugin, type SelectionOptions, ColorAdjustPlugin, type ColorAdjustOptions, MagnificationPlugin, type MagnificationOptions };
+export { MedViewerEngine, type MedEngineOptions, AnnoAnnotator, MedToolbar, type ToolbarOptions, SelectionPlugin, type SelectionOptions, ColorAdjustPlugin, type ColorAdjustOptions, MagnificationPlugin, type MagnificationOptions, type ScalebarOptions };
 
 // 2. 导出供原生 HTML 全局变量使用 (window.MedViewerSDK)
 if (typeof window !== 'undefined') {
